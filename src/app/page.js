0db2cb0a31633f192cf16ce9d263f0a8e01ff0c7
Ronaldo Monserrate <ronsml1594@gmail.com>
2024-11-07 16:41:08 -0500

    Change title

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,22 @@
-"use client"
-import { CardList } from './components/CardList/CardList';
-import { Error } from './components/Error/Error';
-import { Loading } from './components/Loading/Loading';
+"use client";
+import { CardList } from "./components/CardList/CardList";
+import { Error } from "./components/Error/Error";
+import { Loading } from "./components/Loading/Loading";
 
-import { useFetch } from './components/hooks/useFetch';
+import { useFetch } from "./components/hooks/useFetch";
 
-import styles from './page.module.css';
+import styles from "./page.module.css";
 
 export default function Home() {
-  const url = '/api/commit';
+  const url = "/api/commit";
   const [isLoading, data, error] = useFetch(url);
 
   return (
     <main className={styles.main}>
-      <h2 className={styles.title}>Commit History</h2>
+      <h2 className={styles.title}>Commit History App</h2>
       {error && <Error error={error} />}
       {isLoading && <Loading />}
       {data && <CardList data={data} />}
     </main>
-  )
+  );
 }
